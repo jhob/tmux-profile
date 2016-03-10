94fae4ef741e430ee968a6e3848ec820a262aeff
Joey Hobbs <joey@goodeggs.com>
2016-03-10 12:12:54 -0800

    Adding helpers. Updating README. Enabling `~/.tmux-profiles` as directory for custom profiles.

--- README.md
+++ README.md
@@ -1,8 +1,40 @@
 tmux profile
 ------------
 
-tmux-profile is a small script to quickly configure and initialize 
+tmux-profile is a small script to quickly configure and initialize
 [tmux](http://tmux.sourceforge.net/) sessions.
+
+
+Installation
+------------
+
+Install tmux-profile somewhere
+
+    git clone git@github.com:jhob/tmux-profile.git
+
+Create a `~/.tmux-profiles` directory for your custom profiles.
+
+    mkdir ~/.tmux-profiles
+
+To make it convenient, alias the `load-tmux-profile.rb` script. E.g. append the following to your `~/.bashrc`:
+
+    alias ltp="ruby /path/to/tmux-profile/load-tmux-profile.rb"
+
+
+Usage
+-----
+
+List all profiles:
+
+    ltp -l
+
+Load example profile:
+
+    ltp example
+
+
+Example
+-------
 
 A profile is YAML file which details sessions, windows, and panes and allows
 shell commands and keystrokes to be sent to each.
@@ -11,38 +43,42 @@ Consider an example.yaml:
 
     description: An example profile.
     sessions:
-        - name: example
-          attach: true
-          dir: /tmp/
-          windows:
-            - name: hello
-              cmd: vim hello_world.rb
-              send: g n
-              panes:
-                  - split: v
-                    size: 10
-                    cmd: git status
-            - name: server
-              cmd: ssh foo@bar 
+      - name: example
+        attach: true
+        dir: /tmp/
+        windows:
+          - *editor
+          - name: staging
+            cmd: ssh foo@staging-server-1
+            panes:
+              - split: horizontal
+                cmd: ssh foo@staging-server-2
+          - name: production
+            cmd: ssh bar@production-server-1
+            panes:
+              - split: horizontal
+                cmd: ssh bar@production-server-2
 
-If this file is stored within the profiles directory, then the profile can be
+If this file is stored within your `~/.tmux-profiles` directory, then the profile can be
 initialized like so:
 
 ruby load-tmux-profile.rb example
 
-Running this will create (and attach) a single tmux session named 'example', 
-with 2 windows, the first of which will have 2 panes. One pane will open a file 
-in vim, the other show the git status. The second window will ssh into server 
-*bar* as user *foo*. 
+Running this will create (and attach) a single tmux session named 'example',
+with 3 windows:
+
+1. the first of which will have 3 panes. One pane will open a file
+   in vim, the other two show `git status` and `git log`. This is referencing a
+   helper named `editor`.
+2. The second window will have two splits, each `ssh`'ing into different
+   machines.
+3. The third is much like the second, but into different machines again.
 
 Hopefully you can create more useful profiles without difficulty!
 
 
-notes
------
+TODO
+----
 
-I find it useful to create an alias for this command on your shell, for quick
-access. For example within a .bashrc file:
-
-    alias ltp="ruby /path/to/load-tmux-profile.rb"
-
+- Make script runnable from within a tmux client without trying to nest. Switch
+  into loaded profile.
--- load-tmux-profile.rb
+++ load-tmux-profile.rb
@@ -5,127 +5,160 @@ require 'optparse'
 
 
 def check_deps
-    raise "Please install tmux" unless `which tmux` != ""
+  raise "Please install tmux" unless `which tmux` != ""
 end
 
 
 def session_exists? name
-    system "tmux has-session -t #{name} 2> /dev/null"
+  system "tmux has-session -t #{name} 2> /dev/null"
 end
 
 
 # Runs command in current shell
 def run cmd, args=[]
-    cmd = "#{cmd} #{args.join ' '}".strip
-    puts cmd
-    `#{cmd}`
+  cmd = "#{cmd} #{args.join ' '}".strip
+  puts cmd
+  `#{cmd}`
 end
 
 
 # Sends keys to tmux pane
 def send_to_pane pane, keys
-    keys = [ keys ] unless keys.is_a? Array
-    keys = keys.map { |key|
-        "'#{key}'".gsub /'''/, '"\'"'
-    }
-    run "tmux send-keys", ["-t #{pane}", keys.join(' ')]
+  keys = [ keys ] unless keys.is_a? Array
+  keys = keys.map { |key|
+    "'#{key}'".gsub /'''/, '"\'"'
+  }
+  run "tmux send-keys", ["-t #{pane}", keys.join(' ')]
 end
 
 
 # Runs a command within shell inside tmux
 def run_in_pane pane, cmds
-    cmds = [ cmds ] unless cmds.is_a? Array
-    cmds.each do |cmd|
-        keys = cmd.split("")
-        keys << "Enter"
-        send_to_pane pane, keys
-    end
+  cmds = [ cmds ] unless cmds.is_a? Array
+  cmds.each do |cmd|
+    keys = cmd.split("")
+    keys << "Enter"
+    send_to_pane pane, keys
+  end
+end
+
+def helper_dir
+  File.expand_path('./helpers', File.dirname(__FILE__))
 end
 
 def profile_dir
-    File.expand_path('./profiles', File.dirname(__FILE__))
+  File.expand_path('~/.tmux-profiles', File.dirname(__FILE__))
 end
 
+def system_profile_dir
+  File.expand_path('./profiles', File.dirname(__FILE__))
+end
+
+def load_helpers
+  out = ''
+  Dir.glob "#{helper_dir}/*.yaml" do |filename|
+    key = File.basename(filename).gsub '.yaml', ''
+    out = "helper_#{key}: &#{key}\n"
+    File.readlines(filename).each do |line|
+      out += '  ' + line
+    end
+  end
+  out
+end
 
 # Loads profile by name
 def load_profile profile_name
 
-    default_window = { "name" => "default" }
+  default_window = { "name" => "default" }
+  yaml_data = ''
 
+  yaml_data += load_helpers
+
+  begin
+    yaml_data +=  File.read "#{profile_dir}/#{profile_name}.yaml"
+  rescue
     begin
-        profile = YAML.load_file "#{profile_dir}/#{profile_name}.yaml"
+      yaml_data +=  File.read "#{system_profile_dir}/#{profile_name}.yaml"
     rescue
-        raise "Profile '#{profile_name}' doesn't exist"
+      raise "Profile '#{profile_name}' doesn't exist. Is it in your '~/.tmux-profiles' directory?"
+    end
+  end
+
+  begin
+    profile = YAML.load yaml_data
+  rescue Exception => e
+    raise e
+    raise "Profile '#{profile_name}' isn't valid YAML"
+  end
+
+  # initialize all sessions
+  profile["sessions"].each do |session|
+
+    if session_exists? session["name"]
+      puts "Session '#{session["name"]}' already exists. Skipping."
+      next
     end
 
-    # initialize all sessions
-    profile["sessions"].each do |session|
+    window = session["windows"].first || default_window
 
-        if session_exists? session["name"]
-            puts "Session '#{session["name"]}' already exists. Skipping."
-            next
-        end
+    # get current terminal height/width
+    w = `tput cols`.strip
+    h = `tput lines`.strip
 
-        window = session["windows"].first || default_window
+    # create session
+    current_dir = session["dir"] || window["dir"]
+    args = []
+    args << "-s #{session["name"]}"
+    args << "-n #{window["name"]}"
+    args << "-c #{current_dir}" unless current_dir.nil?
+    args << "-x #{w}"
+    args << "-y #{h}"
+    args << "-d"
+    run "tmux new-session", args
 
-        # get current terminal height/width
-        w = `tput cols`.strip
-        h = `tput lines`.strip
+    # create more windows
+    session["windows"][1..-1].each do |window|
+      current_dir = window["dir"] || session["dir"]
+      args = []
+      args << "-t #{session["name"]}"
+      args << "-n #{window["name"]}"
+      args << "-c #{current_dir}" unless current_dir.nil?
+      args << "-d"
+      run "tmux new-window", args
+    end
 
-        # create session
-        current_dir = session["dir"] || window["dir"]
+    # initialize windows
+    session["windows"].each do |window|
+      n = "#{session["name"]}:#{window["name"]}"
+
+      run_in_pane n, window["cmd"] unless window["cmd"].nil?
+      send_to_pane n, window["send"] unless window["send"].nil?
+
+      panes = window["panes"] || []
+      panes.each do |pane|
+        current_dir = pane["dir"] || window["dir"] || session["dir"]
         args = []
-        args << "-s #{session["name"]}"
-        args << "-n #{window["name"]}"
+        args << "-t #{n}"
         args << "-c #{current_dir}" unless current_dir.nil?
-        args << "-x #{w}"
-        args << "-y #{h}"
-        args << "-d"
-        run "tmux new-session", args
-
-        # create more windows
-        session["windows"][1..-1].each do |window|
-            current_dir = window["dir"] || session["dir"]
-            args = []
-            args << "-t #{session["name"]}"
-            args << "-n #{window["name"]}"
-            args << "-c #{current_dir}" unless current_dir.nil?
-            args << "-d"
-            run "tmux new-window", args
-        end
-
-        # initialize windows
-        session["windows"].each do |window|
-            n = "#{session["name"]}:#{window["name"]}"
-
-            run_in_pane n, window["cmd"] unless window["cmd"].nil?
-            send_to_pane n, window["send"] unless window["send"].nil?
-
-            panes = window["panes"] || []
-            panes.each do |pane|
-                current_dir = pane["dir"] || window["dir"] || session["dir"]
-                args = []
-                args << "-t #{n}"
-                args << "-c #{current_dir}" unless current_dir.nil?
-                args << "-#{ pane["split"][0] || "h" } "
-                args << "-l #{pane["size"]} " unless pane["size"].nil?
-                run "tmux split-window", args
-                cmds = pane["cmd"]
-                run_in_pane n, cmds unless cmds.nil?
-                send = pane["send"]
-                send_to_pane n, send unless send.nil?
-            end
-        end
-
+        args << "-#{ pane["split"][0] || "h" } "
+        args << "-l #{pane["size"]} " unless pane["size"].nil?
+        run "tmux split-window", args
+        cmds = pane["cmd"]
+        run_in_pane n, cmds unless cmds.nil?
+        send = pane["send"]
+        send_to_pane n, send unless send.nil?
+      end
     end
 
-    # attach first specified session
-    profile["sessions"].each do |session|
-        if session["attach"]
-            run "tmux attach", ["-t #{session["name"]}"]
-            break
-        end
+  end
+
+  # attach first specified session
+  profile["sessions"].each do |session|
+    if session["attach"]
+      run "tmux attach", ["-t #{session["name"]}"]
+      break
     end
+  end
 
 end
 
@@ -144,19 +177,19 @@ end
 
 if __FILE__ == $0
 
-    check_deps()
-    parser.parse! ARGV
+  check_deps()
+  parser.parse! ARGV
 
-    if options[:list]
-        puts Dir.new(profile_dir)
-                .select { |f| f =~ /\.yaml$/ }
-                .map { |f| f.sub ".yaml", "" }
-                .join "\n"
-    elsif ARGV.length > 0
-		run "tmux start-server"
-        load_profile ARGV.first
-    else
-        puts parser
-    end
+  if options[:list]
+    puts Dir.new(profile_dir)
+    .select { |f| f =~ /\.yaml$/ }
+    .map { |f| f.sub ".yaml", "" }
+    .join "\n"
+  elsif ARGV.length > 0
+    run "tmux start-server"
+    load_profile ARGV.first
+  else
+    puts parser
+  end
 end
 
